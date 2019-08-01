55549d80a6edf080cd9044195a7dc459183bc4bd
Angela Hanshaw <angela.hanshaw@gmail.com>
2019-07-31 21:40:38 -0500

    add company page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -33,4 +33,18 @@ $(document).ready(function() {
             }
         ]
     });
+
+    //smooth scroll
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+
+        $('html, body').animate(
+            {
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
+            750,
+            'linear'
+        )
+    })
+
 });
