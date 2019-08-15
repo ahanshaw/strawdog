7ff20913d2bdf61db10ce492fe584f6ecbc1af93
Angela Hanshaw <angela.hanshaw@gmail.com>
2019-08-14 21:38:35 -0500

    mobile menu tweaks

--- src/js/global/mobileMenu.js
+++ src/js/global/mobileMenu.js
@@ -1,16 +1,20 @@
 function mobileMenu() {
     var mobileNav = $('.c-header__links'),
+        mobileClose = $('.c-header__mobile--close'),
         page = $('#page');
 
     $('.mobile-open').click(function() {
         page.addClass('fixed');
         mobileNav.animate({
             'left': 0
+        }, function() {
+            mobileClose.addClass('fixed');
         });
     });
 
     $('.mobile-close').click(function() {
         page.removeClass('fixed');
+        mobileClose.removeClass('fixed');
         mobileNav.animate({
             'left': '-100%'
         });
