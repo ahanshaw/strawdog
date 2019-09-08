9fd08bcc9609e21ac039853ebd4629d5cb184a8f
Angela Hanshaw <angela.hanshaw@gmail.com>
2019-09-08 16:17:53 -0500

    add email validation msg

--- src/js/global/footerSubscribeValidation.js
+++ src/js/global/footerSubscribeValidation.js
@@ -1,11 +1,16 @@
 function footerSubscribeValidation() {
     var emailValidation = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i,
         emailInput = $('.c-footer__newsletter input[type=email]'),
-        emailSubmit = $('.c-footer__newsletter input[type=submit]');
+        emailSubmit = $('.c-footer__newsletter input[type=submit]'),
+        error = $('.c-footer__newsletter .error');
 
     emailInput.change(function() {
         if (emailValidation.test(emailInput.val())) {
             emailSubmit.removeAttr('disabled');
+            error.hide();
+        }
+        else {
+            error.show();
         }
     });
 }
